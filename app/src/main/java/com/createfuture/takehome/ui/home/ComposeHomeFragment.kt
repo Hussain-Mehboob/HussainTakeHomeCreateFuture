123605289a0d15bada452dba3e2b2d78363b418a
Hussain Mehboob <hussain.mehboob@mset.io>
2025-05-02 00:21:12 +0100

    [Code] - Reformatted code in HomeFragment to make it readable.

--- app/src/main/java/com/createfuture/takehome/ui/home/ComposeHomeFragment.kt
+++ app/src/main/java/com/createfuture/takehome/ui/home/ComposeHomeFragment.kt
@@ -22,9 +22,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.google.gson.GsonBuilder
 import com.createfuture.takehome.R
 import com.createfuture.takehome.models.ApiCharacter
+import com.google.gson.GsonBuilder
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import retrofit2.Response
@@ -34,12 +34,16 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 
 class ComposeHomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): ComposeView = ComposeView(requireContext()).apply {
-        var retrofit = Retrofit.Builder().baseUrl("https://yj8ke8qonl.execute-api.eu-west-1.amazonaws.com").addConverterFactory(GsonConverterFactory.create(GsonBuilder().create())).client(OkHttpClient.Builder().build()).build()
+        var retrofit =
+            Retrofit.Builder().baseUrl("https://yj8ke8qonl.execute-api.eu-west-1.amazonaws.com")
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .client(OkHttpClient.Builder().build()).build()
         var service = retrofit.create(Service::class.java)
         var charactersBody = mutableStateOf<List<ApiCharacter>?>(null)
         viewLifecycleOwner.lifecycleScope.launch {
@@ -47,29 +51,49 @@ class ComposeHomeFragment : Fragment() {
             charactersBody.value = _characters.body()!!
         }
         setContent {
-            Column(modifier = Modifier.fillMaxSize().paint(painterResource(id = R.drawable.img_characters), contentScale = ContentScale.FillBounds).verticalScroll(rememberScrollState())) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .paint(
+                        painterResource(id = R.drawable.img_characters),
+                        contentScale = ContentScale.FillBounds
+                    )
+                    .verticalScroll(rememberScrollState())
+            ) {
                 if (charactersBody.value != null) {
                     for (character in charactersBody.value!!) {
                         Row {
                             Spacer(modifier = Modifier.size(16.dp))
                             Column {
-                                Text(text = character.name,color = Color.White,fontSize = 16.sp)
+                                Text(text = character.name, color = Color.White, fontSize = 16.sp)
                                 Row {
-                                    Text(text = "Culture: ",color = Color.White,fontSize = 16.sp)
-                                    Text(text = character.culture,color = Color.White,fontSize = 16.sp)
+                                    Text(text = "Culture: ", color = Color.White, fontSize = 16.sp)
+                                    Text(
+                                        text = character.culture,
+                                        color = Color.White,
+                                        fontSize = 16.sp
+                                    )
                                 }
                                 Row {
-                                    Text("Born: ",color = Color.White,fontSize = 16.sp)
-                                    Text(text = character.born,color = Color.White,fontSize = 16.sp)
+                                    Text("Born: ", color = Color.White, fontSize = 16.sp)
+                                    Text(
+                                        text = character.born,
+                                        color = Color.White,
+                                        fontSize = 16.sp
+                                    )
                                 }
                                 Row {
-                                    Text(text = "Died: ",color = Color.White,fontSize = 16.sp)
-                                    Text(text = character.died,color = Color.White,fontSize = 16.sp)
+                                    Text(text = "Died: ", color = Color.White, fontSize = 16.sp)
+                                    Text(
+                                        text = character.died,
+                                        color = Color.White,
+                                        fontSize = 16.sp
+                                    )
                                 }
                             }
                             Spacer(modifier = Modifier.weight(1f))
                             Column {
-                                Text("Seasons: ",color = Color.White,fontSize = 14.sp)
+                                Text("Seasons: ", color = Color.White, fontSize = 14.sp)
                                 var seasons = character.tvSeries.joinToString {
                                     when (it) {
                                         "Season 1" -> "I "
@@ -83,7 +107,7 @@ class ComposeHomeFragment : Fragment() {
                                         else -> ""
                                     }
                                 }
-                                Text(seasons,color = Color.White,fontSize = 14.sp)
+                                Text(seasons, color = Color.White, fontSize = 14.sp)
                             }
                         }
                         Spacer(modifier = Modifier.size(18.dp))
